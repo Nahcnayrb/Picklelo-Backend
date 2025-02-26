6baf51ad97b507307c1a8099dd75f35973568190
bryan <bryanchan22tr@gmail.com>
2025-02-25 21:57:24 -0800

    added support for 2v2

--- src/dtos/CreateDuel.dto.ts
+++ src/dtos/CreateDuel.dto.ts
@@ -3,12 +3,13 @@ import type { WithId, Document, ObjectId } from 'mongodb'
 
 export interface CreateDuelDto extends WithId<Document> {
     _id: ObjectId;
-    higherEloUsername: string;
-    lowerEloUsername: string;
+    higherEloUsername: string[];
+    lowerEloUsername: string[];
     lowerEloGainPotential: number;
     higherEloGainPotential: number;
     higherEloScore: number;
     lowerEloScore: number;
     date: Date;
     videoUrl: string;
+    isDoublesMatch: boolean;
 }
